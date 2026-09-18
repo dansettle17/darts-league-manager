3f09966c833095b71bd40655f09fccb986a25934
Dan Settle <dansettle17@gmail.com>
2026-09-18 14:43:33 +0100

    Update including cognito stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Amplify } from 'aws-amplify'
+
+// Configure AWS Cognito Auth
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'eu-north-1_Vrjy2uuu0', // e.g., eu-west-2_xxxxxx
+      userPoolClientId: '55bonh2athid93ejepomjbig3c', // e.g., 1a2b3c4d5e...
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
 
 function App() {
   return (
